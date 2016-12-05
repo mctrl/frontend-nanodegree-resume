b78277657d2388327e9223039b321ce93e2f5a21
Marty <marty@example.com>
2016-12-05 18:01:17 +0000

    replacing strings and show it on screen

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,13 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append(["Mrt"]);
+ // $("#main").append(["Mrt"]);
+
+ var awesomeThoughts = "I am Mrt and I am AWESOME!";
+
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+ console.log(awesomeThoughts);
+ console.log(funThoughts);
+
+  $("#main").append([funThoughts]);
