4991d5d84a34f2c322049de6f7589454b65b37a7
Marty <marty@example.com>
2016-12-05 18:08:13 +0000

    format name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,11 +4,19 @@ This is empty on purpose! Your code to build the resume will go here.
 
  // $("#main").append(["Mrt"]);
 
- var awesomeThoughts = "I am Mrt and I am AWESOME!";
+ // var awesomeThoughts = "I am Mrt and I am AWESOME!";
 
- var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+ // var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 
- console.log(awesomeThoughts);
- console.log(funThoughts);
+ // console.log(awesomeThoughts);
+ // console.log(funThoughts);
 
-  $("#main").append([funThoughts]);
+  // $("#main").append([funThoughts]);
+
+  var formattedName = HTMLheaderName.replace("%data%", "Martina");
+
+  var formattedRole = HTMLheaderRole.replace("%data%", "Something");
+
+  $("#header").append([formattedRole]);
+
+  $("#header").prepend([formattedName]);
