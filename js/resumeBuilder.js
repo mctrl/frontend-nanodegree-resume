8fbce3050d70135f2c80bbbbe7b79a5f283cbbda
Marty <mctrl@users.noreply.github.com>
2016-12-07 22:48:25 +0100

    display all bio info in the page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,28 +17,43 @@ This is empty on purpose! Your code to build the resume will go here.
 var skills = ["", ""]
 
 var bio = {
-    "name": "Marty",
-    "role": "Something",
-    "contacts": {
-        "mobile": "",
-        "email": "",
-        "github": "",
-        "twitter": "",
-        "location": ""
-    },
-    "welcomeMessage": "",
-    "skills": ["", ""],
-    "biopic": "images/fry.jpg",
-    "display": function() {
-        var formattedName = HTMLheaderName.replace("%data%", this.name);
+	"name": "Marty",
+	"role": "Something",
+	"contacts": {
+		"mobile": "000",
+		"email": "myemail@email.com",
+		"github": "www.mygithub.com",
+		"twitter": "twitterUsername",
+		"location": "Honolulu"
+	},
+	"welcomeMessage": "Hi, welcome to my portfolio",
+	"skills": ["skill1", "skill2"],
+	"biopic": "images/fry.jpg",
+	"display": function() {
+		var formattedName = HTMLheaderName.replace("%data%", this.name);
 		var formattedRole = HTMLheaderRole.replace("%data%", this.role);
 		var formattedImg = HTMLbioPic.replace("%data%", this.biopic);
 
-        $("#header").prepend([formattedRole]);
-        $("#header").prepend([formattedName]);
-        $("#header").prepend([formattedImg]);
+		var formattedMobile = HTMLmobile.replace("%data%", this.contacts.mobile);
+		var formattedEmail = HTMLemail.replace("%data%", this.contacts.email);
+		var formattedTwitter = HTMLtwitter.replace("%data%", this.contacts.twitter);
+		var formattedGithub = HTMLgithub.replace("%data%", this.contacts.github);
+		var formattedLocation = HTMLlocation.replace("%data%", this.contacts.location);
 
-    }
+		// 		HTMLcontactGeneric.replace("%data%", );
+		var formattedWelcome = HTMLwelcomeMsg.replace("%data%", this.welcomeMessage);
+
+		$("#header").prepend([formattedRole]);
+		$("#header").prepend([formattedName]);
+		$("#header").append([formattedImg]);
+		$("#topContacts").append([formattedMobile, formattedEmail, formattedGithub, formattedTwitter, formattedLocation])
+		$("#header").append([formattedWelcome, HTMLskillsStart]);
+		// var skills = this.skills;
+		for (i = 0; i < this.skills.length; i++) {
+			var formattedSkills = HTMLskills.replace("%data%", this.skills[i]);
+			$("#skills").append([formattedSkills]);
+		}
+	}
 }
 
 bio.display();
