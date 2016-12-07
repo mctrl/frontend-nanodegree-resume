49a41d538256680fe2b13403f7a11dcb02a12e0f
Marty <marty@example.com>
2016-12-07 15:33:30 +0000

    build bio object and started populating

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,21 +2,43 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- // $("#main").append(["Mrt"]);
 
- // var awesomeThoughts = "I am Mrt and I am AWESOME!";
+// $("#main").append(["Mrt"]);
 
- // var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// var awesomeThoughts = "I am Mrt and I am AWESOME!";
 
- // console.log(awesomeThoughts);
- // console.log(funThoughts);
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 
-  // $("#main").append([funThoughts]);
+// console.log(awesomeThoughts);
+// console.log(funThoughts);
 
-  var formattedName = HTMLheaderName.replace("%data%", "Martina");
+// $("#main").append([funThoughts]);
 
-  var formattedRole = HTMLheaderRole.replace("%data%", "Something");
+var skills = ["", ""]
 
-  $("#header").prepend([formattedRole]);
+var bio = {
+    "name": "Marty",
+    "role": "Something",
+    "contacts": {
+        "mobile": "",
+        "email": "",
+        "github": "",
+        "twitter": "",
+        "location": ""
+    },
+    "welcomeMessage": "",
+    "skills": ["", ""],
+    "biopic": "images/fry.jpg",
+    "display": function() {
+        var formattedName = HTMLheaderName.replace("%data%", this.name);
+		var formattedRole = HTMLheaderRole.replace("%data%", this.role);
+		var formattedImg = HTMLbioPic.replace("%data%", this.biopic);
 
-  $("#header").prepend([formattedName]);
+        $("#header").prepend([formattedRole]);
+        $("#header").prepend([formattedName]);
+        $("#header").prepend([formattedImg]);
+
+    }
+}
+
+bio.display();
